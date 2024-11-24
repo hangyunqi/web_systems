95fa60993a5ccf86223b457dfde4533400385370
hangyunqi <hangyunqi@hyron.com>
2024-11-24 16:58:38 +0800

    commit

--- dbaccess.php
+++ dbaccess.php
@@ -5,28 +5,28 @@ $password = 'admin01';
 
 // try-catch
 try{
-    $pdo = new PDO($dsn,$username.$password);
+        $pdo = new PDO($dsn,$username,$password);
 
-    $sql = "select created_at,info from site_view_history order by created_at desc limit 1";
+        $sql = "select created_at, info from site_view_history order by created_at desc limit 1";
 
-    $stmt = $pdo->prepare($sql);
+        $stmt = $pdo->prepare($sql);
 
-    $stmt->execute();
+        $stmt -> execute();
 
-    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $sqlins = "insert into site_view_history (info) values ('insert test from " . exec(hostname) . "')";
+        $sqlins = "insert into site_view_history (info) values ('insert test from " . exec(hostname) . "')";
 
-    $stmtins = $pdo->prepare($sqlins);
+        $stmtins = $pdo->prepare($sqlins);
 
-    $stmtins->execute();
+        $stmtins -> execute();
 
 }catch (PDOException $e) {
 }
 
 function escape1($str)
 {
-	    return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+    return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
 }
 
 ?>
@@ -37,11 +37,12 @@ function escape1($str)
 <title>test page for database access</title>
 </head>
 <body >
-#Add comment by Mamoru Ito
 Last Access Time<br><br>
 <?php foreach ($rec as $a):?>
         <?=escape1($a)?><br>
 <?php endforeach; ?>
+ADD COMMOND BY HANG
 </body>
 </html>
+
 
